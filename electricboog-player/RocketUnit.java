92f66d32e8b6e0cb4badf555864de387397dbb2e
Quintin Roper <quintinroper@gmail.com>
2018-01-23 19:37:53 -0500

    RocketUnit
    Added RocketUnit logic

--- electricboog-player/RocketUnit.java
+++ electricboog-player/RocketUnit.java
@@ -1,9 +1,14 @@
 import bc.*;
 
 public class RocketUnit extends BoogUnit {
+    private static long MAX_HEALTH = 0;
+    private Unit unit;
+    private GameController gc;
 
     public RocketUnit(Unit unit, GameController gc, Grid grid) {
         super(unit, gc, grid);
+        this.unit = unit;
+        this.gc = gc;
     }
 
     /*
@@ -25,6 +30,22 @@ public class RocketUnit extends BoogUnit {
         an action.
     */
     public void action(UnitManager unitManager) {
-
+        if (unit.structureIsBuilt() == 0) {
+            MAX_HEALTH = unit.health();
+        } else if (unit.health() < MAX_HEALTH 
+            || unit.structureGarrison().size() >= unit.structureMaxCapacity() 
+            || gc.round() >= 745) {
+            boolean sent = false;
+            int count = 0;
+            while(count < 16 && !sent) {
+                MapLocation temp = new MapLocation(Planet.Mars, (int) (Math.random() * 21), (int) (Math.random() * 21));
+                if (gc.canLaunchRocket(unit.id(), temp)) {
+                    gc.launchRocket(unit.id(), temp);
+                    sent = true;
+                    count = 0;
+                }
+                count++;
+            }
+        }
     }
 }
